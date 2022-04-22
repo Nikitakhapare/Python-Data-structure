e2b3aad4d21a546962378660fcf901aead7fe3a5
Nikita <nikitakhapare53@gmail.com>
2022-04-22 16:20:36 +0530

    [nikita]Add:Python program to get last part of string at specific formate

---
+++ String/8_lastPartOfString.py
@@ -0,0 +1,14 @@
+'''
+@Author: Nikita
+@Date: 2022-04-18 11: 55: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-18 11:55:00
+@Title: Write a Python program to get the last part of a string before a specified character.
+'''
+
+
+text="https://www.w3resource.com/python-exercise"
+print(text.rsplit('-',1))
+
+txt="https://www.w3resource.com/python-exercise"
+print(txt.rsplit(':',1))
