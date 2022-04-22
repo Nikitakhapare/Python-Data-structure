2d8913ae7a44c9ca3b09207368b637e98c05be6a
Nikita <nikitakhapare53@gmail.com>
2022-04-22 15:53:50 +0530

    [nikita]Add:Python Program to Create Set

---
+++ Set/1.creatSet.py
@@ -0,0 +1,10 @@
+'''
+@Author: Nikita
+@Date: 2022-04-19 10: 55: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-19 10:55:00
+@Title:Write a Python program for creating set
+'''
+if __name__=="__main__":
+    s={1,2,5,9,5,2,1}
+    print("Sets are: ",s)
