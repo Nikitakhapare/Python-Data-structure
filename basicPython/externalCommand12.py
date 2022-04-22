af00185a273ee7c898546df24fd833be211db986
Nikita <nikitakhapare53@gmail.com>
2022-04-22 13:41:17 +0530

    [nikita]Add:Python Program to Print Dcument of python Built in function

---
+++ basicPython/externalCommand12.py
@@ -0,0 +1,13 @@
+'''
+@Author: Nikita
+@Date: 2022-04-018 05: 45: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-18 05:45:00
+@Title: Basic python Program to print cocuments for built in fuction
+'''
+
+
+from subprocess import call
+
+# call(["ls", "-l"] ,shell=True)
+call(["dir"] ,shell=True)
