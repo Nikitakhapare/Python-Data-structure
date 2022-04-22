96000c6c3aecd264ec3475f182e76a9676af568a
Nikita <nikitakhapare53@gmail.com>
2022-04-22 16:18:27 +0530

    [nikita]Add:Python program to take input from user and conver it into upper and lower case

---
+++ String/convertIntoUpper&Lower6.py
@@ -0,0 +1,13 @@
+'''
+@Author: Nikita
+@Date: 2022-04-18 11: 30: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-18 11:30:00
+@Title:Write a Python script that takes input from the user and displays that input back in
+       upper and lower cases.
+
+'''
+
+user_input = input("What are you looking for ? ")
+print("I am looking for  ", user_input.upper())
+print("I am looking for ", user_input.lower())
