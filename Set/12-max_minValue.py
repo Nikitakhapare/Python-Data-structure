104bd2e8f868ec2d78ded19742a63a05274145aa
Nikita <nikitakhapare53@gmail.com>
2022-04-22 16:00:02 +0530

    [nikita]Add:Python Program to find maximum and minimum value from set

---
+++ Set/12-max_minValue.py
@@ -0,0 +1,14 @@
+'''
+@Author: Nikita
+@Date: 2022-04-20 09: 05: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-20 09:05:00
+@Title:Write a Python program to find maximum and mminimum value of set
+'''
+
+if __name__=="__main__":
+    my_set={1,5,2,6,8}
+    a=max(my_set)
+    b=min(my_set)
+    print("Maximum value of set is: ",a)
+    print("Minimum value of set is: ",b)
