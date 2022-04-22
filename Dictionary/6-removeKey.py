d70185ef3bf493ffd5375ba869ac676cd637907c
Nikita <nikitakhapare53@gmail.com>
2022-04-22 17:02:10 +0530

    [nikita]Add: Python Program to remove key from dictionary

---
+++ Dictionary/6-removeKey.py
@@ -0,0 +1,13 @@
+'''
+@Author: Nikita
+@Date: 2022-04-21 05: 20: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-19 05:20:00
+@Title:Write a Python program to remove a key from a dictionary.
+
+'''
+
+my_dict = {1:"red",2:"green",3:"blue",4:"pink"}
+print("Original Dictionary is : ",my_dict)
+del my_dict[2]
+print("After Removing 2 key Dictionary Become: ",my_dict)
