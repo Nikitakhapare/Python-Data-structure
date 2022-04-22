f186229dbc0e6cfc718a6547579e690101e2f1e4
Nikita <nikitakhapare53@gmail.com>
2022-04-22 16:04:04 +0530

    [nikita]Add:Python Program to create a Tuple with different Data types

---
+++ Touple/2-toupleWithDiffDataType.py
@@ -0,0 +1,12 @@
+'''
+@Author: Nikita
+@Date: 2022-04-19 10: 55: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-19 10:55:00
+@Title:Write a Python program for create Touplewith different data type
+'''
+if __name__=="__main__":
+    myTouple=("red","Green",2,6,8.6)
+    print(type(myTouple))
+    print("Touple is  ",myTouple)
+    
