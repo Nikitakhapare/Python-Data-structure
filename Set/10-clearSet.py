6a39d93964141f2bc080f1d713d3ab5a691efbf3
Nikita <nikitakhapare53@gmail.com>
2022-04-22 15:58:32 +0530

    [nikita]Add:Python Program to clear set

---
+++ Set/10-clearSet.py
@@ -0,0 +1,13 @@
+'''
+@Author: Nikita
+@Date: 2022-04-20 08: 40: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-20 08:40:00
+@Title:Write a Python program to clear set
+'''
+
+if __name__=="__main__":
+    my_set={1,5,2,6,8}
+    my_set.clear()
+    print("Set Become : ",my_set)
+    
