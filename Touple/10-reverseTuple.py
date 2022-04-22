077368d044e5ac34b68cc1d21a7211fec417836b
Nikita <nikitakhapare53@gmail.com>
2022-04-22 16:09:16 +0530

    [nikita]Add:Python Program to reverse a tuple

---
+++ Touple/10-reverseTuple.py
@@ -0,0 +1,16 @@
+'''
+@Author: Nikita
+@Date: 2022-04-20 10: 55: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-20 10:55:00
+@Title:Write a Python program to Reverse a tuple.
+
+'''
+if __name__=="__main__":
+    myTuple=(25,66,9,7,3,11,45)
+    print("Tuple list is: ",myTuple)
+
+    sliceTuple=myTuple[::-1]
+    print("After reverse tuple Become: ",sliceTuple)
+
+
