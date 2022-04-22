07f74daa7e18bebc45f59675d902490e45346bb5
Nikita <nikitakhapare53@gmail.com>
2022-04-22 13:53:10 +0530

    [nikita]Add:Python Program to get the no of occurance of perticular item in Array

---
+++ Array/3-noOfOccurance.py
@@ -0,0 +1,16 @@
+'''
+@Author: Nikita
+@Date: 2022-04-21 14: 30: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-19 14:30:00
+@Title:Write a Python program to get the Number of Occurance of specified element in an array.
+'''
+from array import *
+
+
+if __name__=="__main__":
+    arr=array("i",[1,5,4,1,8,9])
+    print("Array is: ",arr)
+    print("Occurance of 1 in array is: ",arr.count(1))
+
+    
