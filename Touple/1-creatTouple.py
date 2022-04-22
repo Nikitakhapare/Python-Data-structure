898f26ba5bba8b7c6687fee3b4ff7d159657e792
Nikita <nikitakhapare53@gmail.com>
2022-04-22 16:03:34 +0530

    [nikita]Add:Python Program to create a Tuple

---
+++ Touple/1-creatTouple.py
@@ -0,0 +1,12 @@
+'''
+@Author: Nikita
+@Date: 2022-04-19 10: 55: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-19 10:55:00
+@Title:Write a Python program for create Touple
+'''
+if __name__=="__main__":
+    myTouple=("red","Green","Blue","Pink")
+    print(type(myTouple))
+    print("Touple is  ",myTouple)
+    print("Touple first element is : ",myTouple[0])
