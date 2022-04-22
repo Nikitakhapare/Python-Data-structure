3985260db9ed943f53ea5f758cc7319a7d889559
Nikita <nikitakhapare53@gmail.com>
2022-04-22 16:12:57 +0530

    [nikita]Add:Python program to calculate length of string

---
+++ String/stringLen1.py
@@ -0,0 +1,11 @@
+'''
+@Author: Nikita
+@Date: 2022-04-18 07: 10: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-18 07:10:00
+@Title: length of string
+'''
+
+str=input("Enter a String: ")
+stringLen=len(str)
+print("Lenghth of String is: ",stringLen)
