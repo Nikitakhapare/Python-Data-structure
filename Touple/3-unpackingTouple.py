b81264db2e87e182c7c8231f8c0d5a65bb950873
Nikita <nikitakhapare53@gmail.com>
2022-04-22 16:05:04 +0530

    [nikita]Add:Python Program to unpak tuple i seveal variable

---
+++ Touple/3-unpackingTouple.py
@@ -0,0 +1,15 @@
+'''
+@Author: Nikita
+@Date: 2022-04-19 10: 55: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-19 10:55:00
+@Title:Write a Python program for to unpack values of Touple
+'''
+if __name__=="__main__":
+    #These Line Pakes Values of Touple
+    myTouple=("nikita","Khapare",25,)
+    #These Line unpack values of touple
+    (name,last_name,age)=myTouple
+    print(name)
+    print(last_name)
+    
