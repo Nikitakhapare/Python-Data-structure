674d0c22c1806f4b9f335b6406d60be5ab0c5f7b
Nikita <nikitakhapare53@gmail.com>
2022-04-22 17:18:45 +0530

    [nikita]Add: Python Program that takes two lists and returns True if they have common item.

---
+++ List/9-commonitem.py
@@ -0,0 +1,48 @@
+'''
+@Author: Nikita
+@Date: 2022-04-19 16: 10: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-19 15:20:00
+@Title:Write a Python program to find common elements among two lits
+'''
+
+
+def common_elements():
+    """
+        Description:
+            Function to find common items in two lists list
+        Parameter:
+            no parameter
+        Return:
+            returning common elements
+    """ 
+    common=0
+    for i in first_list:
+        if i in second_list:
+            common+=1
+
+        if common==0:
+            return False
+        else:
+            return True
+        
+
+if __name__=="__main__":
+    first_list=[]
+    n=int(input("Enter Number of element in first list: "))
+    for i in range(0,n):
+        first_list.append(int(input()))
+
+    print(first_list)
+
+    second_list=[]
+    n=int(input("Enter Number of element in second list: "))
+    for i in range(0,n):
+        second_list.append(int(input()))
+    print(second_list)
+
+    print(common_elements())
+
+
+    
+    
