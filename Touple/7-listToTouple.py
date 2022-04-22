33861cae3c25b4a31bc764f95d8d0ffe07dd6c39
Nikita <nikitakhapare53@gmail.com>
2022-04-22 16:07:30 +0530

    [nikita]Add:Python Program to check to convert list to tuple

---
+++ Touple/7-listToTouple.py
@@ -0,0 +1,16 @@
+'''
+@Author: Nikita
+@Date: 2022-04-20 10: 55: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-20 10:55:00
+@Title:Write a Python program to check whether an element exists within a tuple.
+
+'''
+if __name__=="__main__":
+    #Declaring List
+    myList=[1,6,8,9,4]
+    print(myList)
+    #converting it into Tuple
+    myList=tuple(myList)
+    print(myList)
+    print(type(myList))
